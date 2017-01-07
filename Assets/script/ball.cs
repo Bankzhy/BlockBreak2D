3c739ee3953d6fe392301f6f71ec50eef676499c
Bankzhy <bankzhy@gmail.com>
2017-01-07 21:33:54 +0900

    BlockBreak2DGame
    BlockBreak2DGame developed by Unity

---
+++ Assets/script/ball.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ball : MonoBehaviour {
+    public float speed = 100f;
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody2D>().velocity = Vector2.down*speed;
+
+
+	}
+	void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "racket_1")
+        {
+            float x = hitFactor(transform.position, col.transform.position, col.collider.bounds.size.x);
+            Vector2 dir = new Vector2(x,1).normalized;
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+    }
+    float hitFactor(Vector2 ballPos,Vector2 racketPos,float racketwidth)
+    {
+        return (ballPos.x - racketPos.x) / racketwidth;
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/script/racket.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class racket : MonoBehaviour {
+    public float speed = 150f;
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * h * speed;
+    }
+}
